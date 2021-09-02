4e831064dec1a5f09fa9bc9a92440562c9ba9247
Yam <yamkkarki@gmail.com>
2021-09-02 14:36:17 +0530

    initial commit

---
+++ src/main/java/com/testjava/test/java/TestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.testjava.test.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaApplication.class, args);
+	}
+
+}
