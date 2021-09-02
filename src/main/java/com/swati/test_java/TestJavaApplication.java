26786935349b49c02d87e408e4cd14df66035d97
Yam <yamkkarki@gmail.com>
2021-09-02 14:54:09 +0530

    structure change

--- src/main/java/com/swati/test_java/TestJavaApplication.java
+++ src/main/java/com/swati/test_java/TestJavaApplication.java
@@ -1,4 +1,4 @@
-package com.testjava.test.java;
+package com.swati.test_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
